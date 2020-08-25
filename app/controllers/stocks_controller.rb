39b1df5c7d743921275dde13380388bb5a5c3ba0
vvdkhoa <vovandangkhoa@gmail.com>
2020-08-25 17:10:40 +0900

    Completed search funtion using ajax

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,14 +3,20 @@ class StocksController < ApplicationController
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
       if @stock
-        render 'users/my_portfolio'
+        respond_to do |format|
+          format.js { render partial: 'users/result'}
+        end
       else
-        flash[:alert] = "Please enter a valid symboy to search"
-        redirect_to my_portfolio_path
+        respond_to do |format|
+          flash.now[:alert] = "Please enter a valid symboy to search"
+          format.js { render partial: 'users/result'}
+        end
       end
     else
-      flash[:alert] = "Please enter a symbol to search"
-      redirect_to my_portfolio_path
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a symboy to search"
+        format.js { render partial: 'users/result'}
+      end
     end
   end
 end
