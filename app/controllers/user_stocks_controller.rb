06f7d31f6ce0bddc479a903819595d2cb86dba01
vvdkhoa <vovandangkhoa@gmail.com>
2020-08-27 20:35:43 +0900

    allow first and last names during signup

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,4 +1,5 @@
 class UserStocksController < ApplicationController
+
   def create
     stock = Stock.check_db(params[:ticker])
     if stock.blank?
@@ -9,4 +10,13 @@ class UserStocksController < ApplicationController
     flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
     redirect_to my_portfolio_path
   end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    user_stock.destroy
+    flash[:notice] = "#{stock.ticker} was sucessfully removed from portfolio"
+    redirect_to my_portfolio_path
+  end
+
 end
